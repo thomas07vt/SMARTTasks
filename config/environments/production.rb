fdc728ebee9aa6947ed209dbaddd702de1e603ff
= <thomas07@vt.edu>
2014-04-28 14:47:32 -0700

    Actually installed Devise, along with default model and views

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Smartgoals::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { host: 'smartgoals.herokuapp.com' }
 end
